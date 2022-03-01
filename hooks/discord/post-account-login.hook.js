958acb72e166eded858017db2a53758cb008c43b
Stephen Cox <stephen.cox@strivacity.com>
2022-03-01 14:18:01 -0500

    add discord integration (#4)

---
+++ hooks/discord/post-account-login.hook.js
@@ -0,0 +1,73 @@
+/**
+ * Discord integration example
+ * 
+ * This code is provided as a sample integration with Discord. Customize to fit your needs.
+ * 
+ * @module integrations/discord
+ */
+
+const discord = require('webhook-discord');
+const dns = require('dns').promises
+
+const DISCORD_WEBHOOK = 'https://[discord webhook]'
+
+/** This function will be called from the Post-Account Login hook in a nonblocking manner.
+ *
+ * @param {Object}   args                                         Input arguments
+ * @param {Object}   args.application                             Application related information
+ * @param {string}   args.application.name                        Name
+ * @param {string}   args.application.client_id                   OAuth client ID
+ * @param {Object}   args.oidc_context                            Information about the originating OpenID Connect request
+ * @param {string[]} args.oidc_context.acr_values                 ACR values
+ * @param {string[]} args.oidc_context.ui_locales                 UI locales
+ * @param {Object}   args.customer                                Customer related information
+ * @param {string}   args.customer.ip_address                     HTTP client IP coming from the X-Forwarded-For header
+ * @param {string[]} args.customer.store                          ID of store containing the customer
+ * @param {string[]} args.customer.info                           map holding stored information about the user
+ * @param {string}   args.customer.info.id                        ID of the customer
+ * @param {Object[]} args.customer.groups                         List of groups the customer is the members of
+ * @param {string}   args.customer.location.city                  City of the customer
+ * @param {string}   args.customer.location.state                 State of the customer
+ * @param {string}   args.customer.location.country               Country of the customer
+ * @param {string}   args.customer.location.country_code          Country code of the customer
+ * @param {string}   args.customer.location.coordinates.latitude  Latitude coordinate of the customer
+ * @param {string}   args.customer.location.coordinates.longitude Longitude coordinate of the customer
+ * @param {Object}   args.session                                 Session store
+ */
+module.exports = async function({ application, oidc_context, customer, session }) {
+    // get ip and reverse dns
+    let ip_address = customer.ip_address
+    const domain = await reverse(ip_address)
+    if (domain) {
+        ip_address += (" (" + domain + ")")
+    }
+
+    // set up discord notifier
+    let hook = new discord.Webhook(DISCORD_WEBHOOK);
+    const msg = new discord.MessageBuilder()
+        .setName("Strivacity")
+        .setTitle("User completed login")
+        .addField("Application Name", application.name, true)
+        .addField("User ID", customer.info.userName, true)
+        .addField("IP", ip_address, true)
+
+    // send off the notification
+    try {
+        await hook.send(msg);
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+/**
+ * Reverse lookup the DNS record for the given IP.
+ */
+async function reverse(ip) {
+    let domain;
+    try {
+        domain = await dns.reverse(ip)
+    } catch (e) {
+        console.log(`failed to look up dns (${e})`)
+    }
+    return domain
+}
